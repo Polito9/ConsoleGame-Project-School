b9367cb1b76208b59dabda781c9e2f7595a3ccd3
Polito9 <colin.reyes.diego@gmail.com>
2023-09-30 16:08:00 -0600

    Starting project

---
+++ main.py
@@ -0,0 +1,13 @@
+#Provitional creation of questions an answers, we're gonna actually create them by reading files
+questions = [["q11", "q12", "q13", "q14", "q15"], 
+             ["q11", "q12", "q13", "q14", "q15"],
+             ["q11", "q12", "q13", "q14", "q15"],
+             ["q11", "q12", "q13", "q14", "q15"],
+             ["q11", "q12", "q13", "q14", "q15"]]
+
+anwers = [[["c1", "in1", "in2", "in3"], ["c1", "in1", "in2", "in3"], ["c1", "in1", "in2", "in3"], ["c1", "in1", "in2", "in3"], ["c1", "in1", "in2", "in3"]],
+          [["c1", "in1", "in2", "in3"], ["c1", "in1", "in2", "in3"], ["c1", "in1", "in2", "in3"], ["c1", "in1", "in2", "in3"], ["c1", "in1", "in2", "in3"]],
+          [["c1", "in1", "in2", "in3"], ["c1", "in1", "in2", "in3"], ["c1", "in1", "in2", "in3"], ["c1", "in1", "in2", "in3"], ["c1", "in1", "in2", "in3"]],
+          [["c1", "in1", "in2", "in3"], ["c1", "in1", "in2", "in3"], ["c1", "in1", "in2", "in3"], ["c1", "in1", "in2", "in3"], ["c1", "in1", "in2", "in3"]],
+          [["c1", "in1", "in2", "in3"], ["c1", "in1", "in2", "in3"], ["c1", "in1", "in2", "in3"], ["c1", "in1", "in2", "in3"], ["c1", "in1", "in2", "in3"]]]
+
